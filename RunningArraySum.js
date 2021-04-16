09e14fd1f7a624c2f6c9faaae89a79082002e5f0
Shawn Gay <Shawn.Gay.Jr@gmail.com>
2021-04-16 15:21:24 -0500

    Added folders for: easy, hard, medium, unfinished.

---
+++ RunningArraySum.js
@@ -0,0 +1,16 @@
+// Given an array nums. We define a running sum of an 
+// array as runningSum[i] = sum(nums[0]…nums[i]).
+
+// Return the running sum of nums.
+
+
+const runningSum = (numbers) => {
+    
+}
+
+const nums1 = [1,2,3,4]
+const nums2 = [2,3,4,5]
+const nums3 = [1,1,1,1,1]
+
+console.log(runningSum(nums1))
+
